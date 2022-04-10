a0e53be8d611bc782d777228b11bec98dff7f6b5
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-04-10 14:22:35 +0200

    feature(setup) update state page

--- apps/nuxt-app/components/elements/VModelExample.vue
+++ apps/nuxt-app/components/elements/VModelExample.vue
@@ -11,9 +11,8 @@ const newStateValue = ref(null)
         <h2 class="text-3xl font-extrabold tracking-tight text-black-900 sm:text-4xl">
           <div class=" text-lg text-indigo-600">v-model example (dynamic)</div>
           <div class=" text-orange-600 mb-5 py-20">State Is:
-            <span class="text-green-600">
-              {{newStateValue }}
-              <span v-if="newStateValue == null">null</span></span></div>
+            <span class="text-green-600">{{ newStateValue }}</span>
+            <span class="text-green-600" v-if="newStateValue == null">null</span></div>
 
 
           <input v-model="newStateValue"
@@ -30,24 +29,13 @@ const newStateValue = ref(null)
       </div>
     </figure>
 
-    <div
-      class="max-w-3l mx-auto py-6 px-4 sm:px-6 lg:py-6 place-content-center grid-rows-1 grid bg-slate-100 ml-1  mr-5 rounded">
-      <div class="">
+    <div class="max-w-3l mx-auto py-6 px-4 sm:px-6 lg:py-6 place-content-center grid-rows-1 grid bg-slate-100 ml-1  mr-5 rounded">
+      <div class="mt-10">
         <figure class="rounded-xl flex">
           <div class="">
                 <pre>
           <code class="language-javascript">
-import SimpleButton from "~/components/elements/SimpleButton.vue";
-import {useLoggedIn} from "~/composables/useLoggedIn";
-import {useStorage} from "@vueuse/core";
-
-const newStateValue = ref('')
-const {user} = useLoggedIn()
-const storageTestState = useStorage('testStorage', '')
-
-function updateStorageState() {
-  storageTestState.value = newStateValue.value
-}
+const newStateValue = ref(null)
         </code>
         </pre>
 
