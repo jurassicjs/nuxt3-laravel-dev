b8723afaaa4aff3f9bb20e3405a9d9887b8176ae
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-04-10 12:23:34 +0200

    feature(setup) extract vmodel example

---
+++ apps/nuxt-app/components/elements/VModelExample.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+const newStateValue = ref('')
+
+</script>
+
+<template>
+  <div class="bg-gray-50 grid grid-cols-2">
+    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 place-content-center grid-rows-1 grid">
+        <h2 class="text-3xl font-extrabold tracking-tight text-black-900 sm:text-4xl">
+          <div class=" text-lg text-indigo-600">v-model example</div>
+          <div class=" text-orange-600 mb-5 py-20">Test State Is: <span class="text-green-600">{{
+              newStateValue
+            }}</span></div>
+          <input v-model="newStateValue"
+                 class=" border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                 type="text"
+                 placeholder="enter new state value" aria-label="state value">
+        </h2>
+      </div>
+
+    </figure>
+
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 place-content-center grid-rows-1 grid bg-slate-100 ml-1 rounded">
+      <div class="text-xl font-extrabold tracking-tight text-gray-600 sm:text-4xl">
+        <figure class="rounded-xl flex">
+          <div class="flex">
+                <pre>
+          <code class="language-javascript">
+const newStateValue = ref('')
+        </code>
+        </pre>
+
+          </div>
+        </figure>
+        <img class="mx-auto h-50 w-auto"
+             src="~/public/img/vModelTemplateExample.png"
+             alt="full stack jack logo"
+        />
+      </div>
+    </div>
+
+  </div>
+</template>
+
--- apps/nuxt-app/pages/non-persistent-state.vue
+++ apps/nuxt-app/pages/non-persistent-state.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
 import Navbar from "~/components/layout/navbar.vue";
-import ActionButton from "~/components/elements/ActionButton.vue";
 import YouAreHere from "~/components/elements/YouAreHere.vue";
-import {useLoggedIn} from "~/composables/useLoggedIn";
-import {useState} from "#app";
+import VModelExample from "~/components/elements/VModelExample.vue";
 
-const newStateValue = ref('')
-const {user} = useLoggedIn()
-const testState = useState('testState', () => 'unset')
-
-function updateTestState() {
-  testState.value = newStateValue.value
-}
+const vModelExampleValue = ref('')
 
 </script>
-<script lang="ts">
-import Prism from '~/plugins/prism'
+<!--<script lang="ts">-->
+<!--import Prism from '~/plugins/prism'-->
 
-export default {
-  mounted() {
-    Prism.highlightAll()
-  }
-}
-</script>
+<!--export default {-->
+<!--  mounted() {-->
+<!--    Prism.highlightAll()-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 
 
 <template>
@@ -33,28 +25,23 @@ export default {
 
     <YouAreHere/>
 
+<VModelExample/>
+
     <div class="bg-gray-50">
       <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span class="block text-indigo-600">TestState Is: {{ testState }}</span>
+          <span class="block text-indigo-600">Test State Is: {{ vModelExampleValue }}</span>
         </h2>
       </div>
       <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 place-content-center grid-rows-1 grid">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <input v-model="newStateValue"
+          <input v-model="vModelExampleValue"
                  class=" border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text"
                  placeholder="enter new state value" aria-label="state value">
         </h2>
-        <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0 mt-5">
-          <ActionButton
-            actionClasses="bg-emerald-600"
-            title="Update dynamic State"
-            action-link="fake"
-            @action="updateTestState"
-          />
-        </div>
       </div>
     </div>
+
 
     <div class="flex justify-center">
       <pre>
@@ -70,7 +57,7 @@ function updateTestState() {
 }
       </code>
       </pre>
-    </div>
 
+    </div>
   </div>
 </template>
