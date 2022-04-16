2b6cbded13cb69b5bad329c31872940ddd1578d7
Richard Rohrig <richard.t.rohrig@gmail.com>
2022-04-16 23:02:51 +0200

    feature(setup) add tags

--- apps/nuxt-app/server/api/categories.ts
+++ apps/nuxt-app/server/api/categories.ts
@@ -3,11 +3,11 @@ export default () => {
   const rowSize = 2
   const categories = [
     {title: 'Nuxt 3', message: 'Nuxt 3 for beginners course', image: '/public/nuxt3.svg', tags: [{title: 'Front End', link: '/frontend-courses'}]},
-    {title: 'Laravel', message: 'Laravel For beginners course', image: '/public/laravel.svg'},
-    {title: 'PHP', message: 'Ultimate PHP Course', image: '/public/php-logo.svg'},
-    {title: 'Nginx', message: 'Nginx Course for Beginners', image: '/public/nginx.svg'},
-    {title: 'Docker', message: 'Docker Course for Beginners', image: '/public/docker.svg'},
-    {title: 'Linux', message: 'Linux Course for Beginners', image: '/public/linux.svg'},
+    {title: 'Laravel', message: 'Laravel For beginners course', image: '/public/laravel.svg', tags: [{title: 'Bank End', link: '/backend-courses'}]},
+    {title: 'PHP', message: 'Ultimate PHP Course', image: '/public/php-logo.svg', tags: [{title: 'Bank End', link: '/backend-courses'}]},
+    {title: 'Nginx', message: 'Nginx Course for Beginners', image: '/public/nginx.svg', tags: [{title: 'Servers', link: '/server-courses'}]},
+    {title: 'Docker', message: 'Docker Course for Beginners', image: '/public/docker.svg', tags: [{title: 'containerization', link: '/containerization-courses'}]},
+    {title: 'Linux', message: 'Linux Course for Beginners', image: '/public/linux.svg', tags: [{title: 'OS', link: '/os-courses'}]},
 
   ]
 
